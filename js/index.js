dd566ebaf951f937a80350603f40ee8d1ec33c3d
diwakar@pay2mobiles.com <diwakarUpadhyay@Diwakars-MacBook-Pro-2.local>
2022-05-14 00:30:44 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -1,15 +1,39 @@
 
 
-$(document).ready(function(){ 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('#scroll').fadeIn(); 
-        } else { 
-            $('#scroll').fadeOut(); 
-        } 
-    }); 
-    $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
-});
+// $(document).ready(function(){ 
+//     $(window).scroll(function(){ 
+//         if ($(this).scrollTop() > 100) { 
+//             $('#scroll').fadeIn(); 
+//         } else { 
+//             $('#scroll').fadeOut(); 
+//         } 
+//     }); 
+//     $('#scroll').click(function(){ 
+//         $("html, body").animate({ scrollTop: 0 }, 600); 
+//         return false; 
+//     }); 
+// });
+
+
+// slider js
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    focusOnSelect: true
+  });
+ 
+  $('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.slider-nav').slick('slickGoTo', slideno - 1);
+  });
